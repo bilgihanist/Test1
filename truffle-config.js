65b70a7e6a32809cb458dc9c3abbb02fdd64a33b
bilgihanist <71718784+bilgihanist@users.noreply.github.com>
2022-11-02 11:42:50 +0300

    ders4

--- truffle-config.js
+++ truffle-config.js
@@ -41,10 +41,12 @@
  * https://trufflesuite.com/docs/truffle/getting-started/using-the-truffle-dashboard/
  */
 
-// require('dotenv').config();
-// const { MNEMONIC, PROJECT_ID } = process.env;
+// const mnemonic = require("./secret.json").mnemonic;   // artık bu kullanılmıyor.
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+ require('dotenv').config();                                         // bunu açtık
+ //const { MNEMONIC, PROJECT_ID } = process.env;                     // bunu açtık
+ const mnemonic = "laundry crop wonder change october blood industry inspire double shop farm medal";
+ const HDWalletProvider = require("@truffle/hdwallet-provider");     // bunu açtık
 
 module.exports = {
   /**
@@ -82,13 +84,13 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, `https://goerli.infura.io/v3/771080e84bba40b5822c8498155a5b73`),
+      network_id: 5,       // Goerli's id
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
